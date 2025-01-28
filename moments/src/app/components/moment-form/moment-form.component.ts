6c3df590063642ed2a52519ef9058e7adb3698f0
Domingos Capewa <domingoscapewa08@gmail.com>
2025-01-27 22:57:23 -0300

    Validação de formulário adicionada

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,16 +1,65 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moments } from 'src/app/moments';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.css']
+  styleUrls: ['./moment-form.component.css'],
 })
 export class MomentFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Moments>();
+  @Input() momentData: Moments | null = null;
   @Input() btnText!: string;
 
-  constructor() { }
+  image?: File;
+
+  momentForm!: FormGroup;
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.momentData) {
+      console.log(this.momentData);
+      this.momentForm = new FormGroup({
+        id: new FormControl(this.momentData.id),
+        title: new FormControl(this.momentData.title, [Validators.required]),
+        description: new FormControl(this.momentData.description, [
+          Validators.required,
+        ]),
+        image: new FormControl(''),
+      });
+    } else {
+      this.momentForm = new FormGroup({
+        id: new FormControl(''),
+        title: new FormControl('', [Validators.required]),
+        description: new FormControl('', [Validators.required]),
+        image: new FormControl(''),
+      });
+    }
   }
 
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: event.target.files[0] });
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+
+    console.log(this.momentForm.value);
+
+    this.onSubmit.emit(this.momentForm.value);
+  }
 }
