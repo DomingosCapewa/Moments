cc290a1cd9694d395e3800b48a889618cf0cd12b
Domingos Capewa <domingoscapewa08@gmail.com>
2025-01-27 23:14:24 -0300

    Inserindo dados no sistema

--- moments/src/app/components/pages/new-moments/new-moments.component.ts
+++ moments/src/app/components/pages/new-moments/new-moments.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Moments } from 'src/app/moments';
 
 @Component({
   selector: 'app-new-moments',
   templateUrl: './new-moments.component.html',
-  styleUrls: ['./new-moments.component.css']
+  styleUrls: ['./new-moments.component.css'],
 })
 export class NewMomentsComponent implements OnInit {
-btnText = 'Compartilhar!';
-  constructor() { }
+  btnText = 'Compartilhar!';
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  async createHandler(moment: Moments) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
   }
-
 }
--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Moments } from '../moments';
 
 @Injectable({
   providedIn: 'root'
